671c60bab09bc34097ddf31df547f290728c22cc
jayson7 <lexxiijoo70@gmail.com>
2021-07-13 15:24:07 +0100

    code

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,7 +134,6 @@ STATICFILES_DIRS = (
 )
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
